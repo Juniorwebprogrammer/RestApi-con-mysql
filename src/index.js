7fcb95828ec62573ea94b649b38efaf93ba7e44c
Junior García <juniorgarciadiaz@outlook.com>
2022-06-28 15:53:57 +0200

    inicio del desarrollo de las rutas

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ const app = express();
 // Settings
 
 app.set('port', process.env.PORT || 3000);
+app.set(express.json());
 
 // Server
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
 });
+
+
+// Routes
+
+app.use(require('./routes/employee'));
