291474624dc5e7b5739a310dd8e7f25bcf3b1c9e
Junior García <juniorgarciadiaz@outlook.com>
2022-06-28 15:17:20 +0200

    Inicio del servidor con express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-const {Router} = require ('express');
-const router = Router;
+const express = require ('express');
+
+// Inicialización
+
+const app = express();
+
+// Settings
+
+app.set('port', process.env.PORT || 3000);
+
+// Server
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
